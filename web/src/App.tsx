ecf91707efdb11779c01352fb5b79873ef14d402
Austin Dunn <austindunn.dev@gmail.com>
2024-03-13 00:15:00 -0700

    chore(web): remove print

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -45,7 +45,6 @@ const App: React.FC = () => {
   });
 
   useNuiEvent<InteractionData>("updateInteraction", (newInteraction) => {
-    console.log("update", newInteraction);
     setInteraction(newInteraction);
   });
 
