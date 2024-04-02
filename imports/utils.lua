6a7858286e0a03e648832dc93ae1dc4710fcc195
Austin Dunn <austindunn.dev@gmail.com>
2024-04-01 22:50:27 -0600

    refactor(utils): remove ox inventory dependency

--- imports/utils.lua
+++ imports/utils.lua
@@ -1,5 +1,4 @@
 local globals = require 'imports.globals'
-local Vehicles = require '@ox_inventory.data.vehicles'
 local dui = require 'imports.dui'
 local DuiObject, updateMenu in dui
 local ox_inv = GetResourceState('ox_inventory'):find('start')
@@ -299,32 +298,32 @@ if ox_inv then
     AddEventHandler('ox_inventory:itemCount', function(name, count)
         playerItems[name] = count
     end)
-end
-
-local backDoorIds = { 2, 3 }
-utils.getTrunkPosition = function(entity)
-    local vehicleHash = GetEntityModel(entity)
-    local vehicleClass = GetVehicleClass(entity)
-    local checkVehicle = Vehicles.Storage[vehicleHash]
-
-    if (checkVehicle == 0 or checkVehicle == 1) or (not Vehicles.trunk[vehicleClass] and not Vehicles.trunk.models[vehicleHash]) then return end
-
-    ---@type number | number[]
-    local doorId = checkVehicle and 4 or 5
-
-    if not Vehicles.trunk.boneIndex?[vehicleHash] and not GetIsDoorValid(entity, doorId --[[@as number]]) then
-        if vehicleClass ~= 11 and (doorId ~= 5 or GetEntityBoneIndexByName(entity, 'boot') ~= -1 or not GetIsDoorValid(entity, 2)) then
-            return
+    local Vehicles = require '@ox_inventory.data.vehicles'
+    local backDoorIds = { 2, 3 }
+    utils.getTrunkPosition = function(entity)
+        local vehicleHash = GetEntityModel(entity)
+        local vehicleClass = GetVehicleClass(entity)
+        local checkVehicle = Vehicles.Storage[vehicleHash]
+    
+        if (checkVehicle == 0 or checkVehicle == 1) or (not Vehicles.trunk[vehicleClass] and not Vehicles.trunk.models[vehicleHash]) then return end
+    
+        ---@type number | number[]
+        local doorId = checkVehicle and 4 or 5
+    
+        if not Vehicles.trunk.boneIndex?[vehicleHash] and not GetIsDoorValid(entity, doorId --[[@as number]]) then
+            if vehicleClass ~= 11 and (doorId ~= 5 or GetEntityBoneIndexByName(entity, 'boot') ~= -1 or not GetIsDoorValid(entity, 2)) then
+                return
+            end
+    
+            if vehicleClass ~= 11 then
+                doorId = backDoorIds
+            end
         end
-
-        if vehicleClass ~= 11 then
-            doorId = backDoorIds
-        end
+    
+        local min, max = GetModelDimensions(vehicleHash)
+        local offset = (max - min) * (not checkVehicle and vec3(0.5, 0, 0.5) or vec3(0.5, 1, 0.5)) + min
+        return GetOffsetFromEntityInWorldCoords(entity, offset.x, offset.y, offset.z)
     end
-
-    local min, max = GetModelDimensions(vehicleHash)
-    local offset = (max - min) * (not checkVehicle and vec3(0.5, 0, 0.5) or vec3(0.5, 1, 0.5)) + min
-    return GetOffsetFromEntityInWorldCoords(entity, offset.x, offset.y, offset.z)
 end
 
 utils.clearCacheForInteractionEntity = function (interaction)
