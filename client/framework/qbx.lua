4820dab93f62d522813280734a47d14f15e62a83
unknown <clanerp@gmail.com>
2025-04-19 11:19:59 -0700

    fix(client/framework/qbx): checks only primary group and adds on-duty check

--- client/framework/qbx.lua
+++ client/framework/qbx.lua
@@ -1,9 +1,10 @@
 if not lib.checkDependency('qbx_core', '1.18.0', true) then return end
 
-local QBX = exports.qbx_core
+require '@qbx_core/modules/playerdata.lua'
+local qbx = exports.qbx_core
 local utils = require 'client.modules.utils'
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function utils.hasPlayerGotGroup(filter)
-    return QBX:HasGroup(filter)
+    return qbx:HasPrimaryGroup(filter) and QBX.PlayerData.job.onduty
 end
